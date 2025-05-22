9976ab8ae24a84193f1aadf9e5a7557204df69ab
LiKTOi1029 <testingboy5689@gmail.com>
2025-05-22 13:50:47 +0330

    repeat loop created

--- wordlistgen.lua
+++ wordlistgen.lua
@@ -2,3 +2,8 @@ function FileReader()
 	local input = io.open("input.txt"); local alltext = input:read("*a")
 	
 end
+repeat
+	io.write("Type your desired input into \"input.txt\"\nCopy the output from \"output.txt\" and use it!\nType anything to start")
+	local choice = io.read():gsub("\n","")
+	if choice ~= "EXIT" then FileReader() end
+until choice == "EXIT"
