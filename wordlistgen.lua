eceb729146ce793c6c79bdb0d3279f7ee7dd5fa9
LiKTOi1029 <testingboy5689@gmail.com>
2025-05-25 11:38:44 +0330

    wordlistgen.lua saving progress

--- wordlistgen.lua
+++ wordlistgen.lua
@@ -1,9 +1,14 @@
 function FileReader()
-	local input = io.open("input.txt"); local AllText, SavedText, ParsingText = input:read("*a"), {}, ""
-		for num1 = 1, AllText:len(), 1 do
-			
+	local input = io.open("input.txt"); local AllText, SavedText, ParsingText, Len = input:read("*a"), {}, "", AllText:len()
+	for num1 = 1, Len, 1 do
+		if AllText:sub(num1,num1) == AllText:sub(Len,Len) or AllText:sub(num1,num1) == " " then
+			table.insert(SavedText, ParsingText)
+			ParsingText = ""
+		else
+			ParsingText = ParsingText..AllText:sub(num1,num1)
 		end
 	end
+end
 repeat
 	io.write("Type your desired input into \"input.txt\"\nCopy the output from \"output.txt\" and use it!\nType anything to start")
 	local choice = io.read():gsub("\n","")
